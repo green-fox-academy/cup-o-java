0cea6ec3c5af9f3a755b730fdb212d68523a27e4
Laura Fulmer <laurahajnalka@gmail.com>
2018-07-25 19:51:52 +0200

    add(draw)

--- branching/src/Main.java
+++ branching/src/Main.java
@@ -3,4 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public void draw(String shape) {
+        System.out.println("I am going to draw a " + shape);
+    }
 }

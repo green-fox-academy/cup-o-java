ff50845c9ba4acf3df9653c4015672be4187eca0
Adél Deichler <deichleradel@gmail.com>
2018-07-25 20:02:54 +0200

    a1

--- branching/src/Main.java
+++ branching/src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+        greet("Adél");
+
     }
+        public static void greet(String name){
+            System.out.println("Hello " + name);
+        }
+
+
 }

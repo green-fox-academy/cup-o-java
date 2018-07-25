7a68624a30fa24d67e9c1550e147994b39c0b789
Fellner Sára <sara.fellner@loxon.eu>
2018-07-25 20:24:42 +0200

    c2

--- branching/src/Main.java
+++ branching/src/Main.java
@@ -15,16 +15,12 @@ public class Main {
     }
 
 
-
-
-
-
     public static void draw(String shape) {
         System.out.println("I am going to draw a " + shape);
     }
 
-    public static void cheer() {
-        System.out.println("Cup O' Java");
+    public static void cheer(String writeThis) {
+        System.out.println(writeThis);
 
     }
         public static void greet(String name){

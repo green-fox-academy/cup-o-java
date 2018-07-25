1408b52a86a8adaeb7c7c4db99ea044f65b1cc60
Laura Fulmer <laurahajnalka@gmail.com>
2018-07-25 20:03:01 +0200

    e1

--- branching/src/Main.java
+++ branching/src/Main.java
@@ -2,9 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        draw("diamond");
     }
 
-    public void draw(String shape) {
-        System.out.println("I am going to draw a " + shape);
+        public static void draw(String shape){
+            System.out.println("I am going to draw a " + shape);
+        }
     }
-}
+

169678dc9027e447ce3acb8d3fa20d192eae4a51
Laura Fulmer <laurahajnalka@gmail.com>
2018-07-25 20:24:28 +0200

    e4

--- branching/src/Main.java
+++ branching/src/Main.java
@@ -5,8 +5,8 @@ public class Main {
         count();
 
         draw("diamond");
-
         greet("Adél");
+        cheer();
     }
 
     public static void count() {
@@ -14,22 +14,15 @@ public class Main {
         System.out.println(age);
     }
 
-
-
-
-
-
     public static void draw(String shape) {
         System.out.println("I am going to draw a " + shape);
     }
 
     public static void cheer() {
         System.out.println("Cup O' Java");
-
     }
-        public static void greet(String name){
-            System.out.println("Hello " + name);
-        }
 
-
+    public static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
 }

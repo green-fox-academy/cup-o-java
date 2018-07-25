2be8a7f7801c45d92702c600e553c6b7cf1f3d25
lilla <lilla@quince.hu>
2018-07-25 19:59:12 +0200

    add zodiac function

--- branching/src/Main.java
+++ branching/src/Main.java
@@ -2,5 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
     }
+
+
+
+    public void future() {
+		String zodiac = "Scorpio";
+    	System.out.println("My zodiac is " + zodiac);
+	}
 }
